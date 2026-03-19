801cffb0bc324ab123163bbfbb1a7d0d00cdb715
klimkeurpro <josvdhoogen@gmail.com>
2026-03-19 16:05:31 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,6 +4,6 @@
 // ============================================================
 
 const SUPABASE_URL = 'https://vyptkeqcibtgyrnrcxrj.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5cHRrZXFjaWJ0Z3lybnJjeHJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1NzExMTcsImV4cCI6MjA4ODE0NzExN30.BnzphDOgDB038qraKao2Lx5SpAz4oCSMBWDvNzeIIaM';
+const SUPABASE_ANON_KEY = 'sb_publishable_94HAHA4s4elNEiP3-Mmz3g_UZGd4pgu';
 
 const sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
